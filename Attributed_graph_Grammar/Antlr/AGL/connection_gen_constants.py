81febd5dc88dc785451c7b0bc6b4d7aa1f1b0d49
Satya Amarkant Marupureddy <amarkant23394@gmail.com>
2023-01-29 13:40:07 -0500

    Added constants for relative programming and also added trojan detection environment in zip format

---
+++ Attributed_graph_Grammar/Antlr/AGL/connection_gen_constants.py
@@ -0,0 +1,6 @@
+class ConnectionConstants:
+    single_input_gates = ["NOT","BUF","INV","CLKBUF","INPUT"]
+    three_input_gates = ["DFF"]
+    four_input_gates = ["SDFF"]
+    clockPattern="CLK|clock|clk|CK"
+    resetPattern="rst|reset|RST|Rst|RN|RSTB"
